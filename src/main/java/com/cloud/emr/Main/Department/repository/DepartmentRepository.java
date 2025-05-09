b197bb31f1623c33f602cc905bfa622f03032913
kyungtaeChoi <kmam147@gmail.com>
2025-05-10 01:16:52 +0900

    [#78] feat: 회원가입에 사용될 모든 부서 조회 기능 추가

--- src/main/java/com/cloud/emr/Main/Department/repository/DepartmentRepository.java
+++ src/main/java/com/cloud/emr/Main/Department/repository/DepartmentRepository.java
@@ -2,6 +2,8 @@ package com.cloud.emr.Main.Department.repository;
 
 import com.cloud.emr.Main.Department.entity.DepartmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DepartmentRepository extends JpaRepository<DepartmentEntity,Long> {
 }
