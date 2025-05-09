f3b9c105d2c9de94f844b103d42aaf04e242985b
kyungtaeChoi <kmam147@gmail.com>
2025-05-10 01:16:07 +0900

    [#78] chore: 부서 엔티티에 대한 정의 수정 간결하게 정리하여 부서id -> id이런식으로 정리 테이블에서는 정상적으로 분리

--- src/main/java/com/cloud/emr/Main/Department/entity/DepartmentEntity.java
+++ src/main/java/com/cloud/emr/Main/Department/entity/DepartmentEntity.java
@@ -1,23 +1,29 @@
 package com.cloud.emr.Main.Department.entity;
 
 import com.cloud.emr.Main.Department.type.DepartmentType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
+@Entity(name = "department")
 @Getter
 @Setter
 public class DepartmentEntity {
 
     @Id
-    private Long Id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private String departmentName;
+    @Column(unique = true,name = "department_code")
+    private String code; //약어
 
+    @Column(unique = true,name = "department_name")
+    private String name;
+
+    @Column(unique = true,name = "department_eng_name")
+    private String EngName;
+
+    @Column(name = "department_type")
     @Enumerated(EnumType.STRING)
-    private DepartmentType departmentType;
+    private DepartmentType type;
 }
