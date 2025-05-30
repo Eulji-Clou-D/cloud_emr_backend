e9660915a26b745607e3127483894cc174558f68
vambroag <vambroag@gmail.com>
2025-05-30 20:05:21 +0900

    [#96] refactor : HolidayResponse new Constructor [#99]

--- src/main/java/com/cloud/emr/Affair/Holiday/dto/HolidayResponse.java
+++ src/main/java/com/cloud/emr/Affair/Holiday/dto/HolidayResponse.java
@@ -1,13 +1,17 @@
 package com.cloud.emr.Affair.Holiday.dto;
 
+import com.cloud.emr.Affair.Holiday.entity.HolidayEntity;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.beans.ConstructorProperties;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class HolidayResponse {
@@ -16,6 +20,13 @@ public class HolidayResponse {
     private LocalDate holidayDate;
     private Boolean holidayNational;
     private String holidayReason;
+
+    public HolidayResponse(HolidayEntity e) {
+        this.id = e.getId();
+        this.holidayDate = e.getHolidayDate();
+        this.holidayNational = e.getHolidayNational();
+        this.holidayReason = e.getHolidayReason();
+    }
 
 }
 
