7fc56912701988b7f2b78a5c735c558c9e7caa34
vambroag <vambroag@gmail.com>
2025-06-17 23:03:08 +0900

    [#105] feat : made enum type for holiday date types [#104]

---
+++ src/main/java/com/cloud/emr/Affair/Holiday/type/HolidayDateType.java
@@ -0,0 +1,17 @@
+package com.cloud.emr.Affair.Holiday.type;
+
+public enum HolidayDateType {
+
+    DAY,        // 일
+
+    WEEK,       // 주
+
+    MONTH,      // 월
+
+    QUARTER,    // 분기
+
+    YEAR,       // 년
+
+    CUSTOM;     // 사용자 정의 기간
+
+}
