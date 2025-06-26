35e0247e18aff1b66593a123afabd8bcec7a71c0
vambroag <vambroag@gmail.com>
2025-06-26 21:02:15 +0900

    [#107] feat: made ApiResponse return ReponseEntity for more efficiency [#104]

--- src/main/java/com/cloud/emr/Main/Core/common/wrapperfactory/ApiResponse.java
+++ src/main/java/com/cloud/emr/Main/Core/common/wrapperfactory/ApiResponse.java
@@ -2,6 +2,7 @@ package com.cloud.emr.Main.Core.common.wrapperfactory;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 @Builder
@@ -13,66 +14,74 @@ public class ApiResponse<T> {
     private String message;
     private T data;
 
-    public static <T> ApiResponse<T> of(String message) {
-        return ApiResponse.<T>builder()
+    public static ResponseEntity<ApiResponse<Void>> of(String message) {
+        ApiResponse<Void> body = ApiResponse.<Void>builder()
                 .message(message)
                 .build();
+        return ResponseEntity.status(body.getCode()).body(body);
     }
 
-    public static <T> ApiResponse<T> of(T data) {
-        return ApiResponse.<T>builder()
+    public static <T> ResponseEntity<ApiResponse<T>> of(T data) {
+        ApiResponse<T> body = ApiResponse.<T>builder()
                 .message("요청이 성공적으로 처리되었습니다.")
                 .data(data)
                 .build();
+        return ResponseEntity.status(body.getCode()).body(body);
     }
 
-    public static <T> ApiResponse<T> of(T data, String message) {
-        return ApiResponse.<T>builder()
+    public static <T> ResponseEntity<ApiResponse<T>> of(T data, String message) {
+        ApiResponse<T> body = ApiResponse.<T>builder()
                 .message(message)
                 .data(data)
                 .build();
+        return ResponseEntity.status(body.getCode()).body(body);
     }
 
-    public static <T> ApiResponse<T> of(int code, T data, String message) {
-        return ApiResponse.<T>builder()
+    public static <T> ResponseEntity<ApiResponse<T>> of(int code, T data, String message) {
+        ApiResponse<T> body = ApiResponse.<T>builder()
                 .code(code)
                 .message(message)
                 .data(data)
                 .build();
+        return ResponseEntity.status(code).body(body);
     }
 
-    public static <T> ApiResponse<T> fail(String message) {
-        return ApiResponse.<T>builder()
+    public static ResponseEntity<ApiResponse<Void>> fail(String message) {
+        ApiResponse<Void> body = ApiResponse.<Void>builder()
                 .success(false)
                 .code(500)
                 .message(message)
                 .build();
+        return ResponseEntity.status(body.getCode()).body(body);
     }
 
-    public static <T> ApiResponse<T> fail(T data) {
-        return ApiResponse.<T>builder()
+    public static <T> ResponseEntity<ApiResponse<T>> fail(T data) {
+        ApiResponse<T> body = ApiResponse.<T>builder()
                 .success(false)
                 .code(500)
                 .message("요청 처리를 실패했습니다.")
                 .data(data)
                 .build();
+        return ResponseEntity.status(body.getCode()).body(body);
     }
 
-    public static <T> ApiResponse<T> fail(int code, String message) {
-        return ApiResponse.<T>builder()
+    public static ResponseEntity<ApiResponse<Void>> fail(int code, String message) {
+        ApiResponse<Void> body = ApiResponse.<Void>builder()
                 .success(false)
                 .code(code)
                 .message(message)
                 .build();
+        return ResponseEntity.status(code).body(body);
     }
 
-    public static <T> ApiResponse<T> fail(int code, T data, String message) {
-        return ApiResponse.<T>builder()
+    public static <T> ResponseEntity<ApiResponse<T>> fail(int code, T data, String message) {
+        ApiResponse<T> body = ApiResponse.<T>builder()
                 .success(false)
                 .code(code)
                 .message(message)
                 .data(data)
                 .build();
+        return ResponseEntity.status(code).body(body);
     }
 
 }
